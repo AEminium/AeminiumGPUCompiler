1cb9a013da29a0b3aea889cadce25b98c79535c6
Alcides Fonseca <me@alcidesfonseca.com>
2011-03-20 15:52:58 -0400

    Added compiler launcher and shell script.

---
+++ compiler-src/aeminium/gpu/compiler/launcher/AeminiumGPUCompilerLauncher.java
@@ -0,0 +1,51 @@
+package aeminium.gpu.compiler.launcher;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.martiansoftware.jsap.JSAPException;
+import spoon.Launcher;
+import spoon.support.builder.CtResource;
+import spoon.support.builder.support.CtFolderFile;
+
+import aeminium.gpu.compiler.processing.MapLambdaProcessor;
+
+public class AeminiumGPUCompilerLauncher extends Launcher {
+
+	public AeminiumGPUCompilerLauncher(String[] args) throws JSAPException {
+		super(args);
+	}
+	
+	@Override
+	protected List<String> getProcessorTypes() {
+		List<String> l = super.getProcessorTypes();
+		l.add(MapLambdaProcessor.class.getName());
+		return l;
+	}
+
+	@Override
+	protected List<CtResource> getTemplateSources() {
+		List<CtResource> l = super.getTemplateSources();
+		try {
+			l.add(new CtFolderFile(new File("compiler-src/aeminium/gpu/compiler/template")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return l;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			AeminiumGPUCompilerLauncher launcher = new AeminiumGPUCompilerLauncher(args);
+			launcher.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+	
+
+}
