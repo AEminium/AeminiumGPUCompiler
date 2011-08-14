78fc3ad0966f7b2ce84743eb3388ffadac459a29
Alcides Fonseca <me@alcidesfonseca.com>
2011-08-14 22:40:54 +0100

    Trying new decision and added RandomBenchmark.

--- utils/benchmark_decision.sh
+++ utils/benchmark_decision.sh
@@ -11,3 +11,4 @@ export DEBUG=1
 export BENCH=1
 mkdir -p logs
 ./utils/aegpu benchmark.BenchmarkDecisions > logs/benchmark.log
+./utils/aegpu benchmark.RandomBenchmarkDecisions > logs/random_benchmark.log
